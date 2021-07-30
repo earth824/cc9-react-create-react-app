17d96548db0b5488dbcf086435c4543aca8ed22c
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-07-30 17:04:56 +0700

    clear code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
 
-import Hello from './components/Hello';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
 function App() {
-  return (
-    <div>
-      <Header />
-      <Hello name="John" />
-      <Hello name="Jack" />
-      <Hello name="Jane" />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
