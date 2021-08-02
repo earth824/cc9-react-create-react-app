cad2ae7791672d0cf383dd6fe4d12dbac4137063
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-08-02 09:23:46 +0700

    complete inline styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div style={{ padding: '2rem' }}>
+      <button
+        style={{
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: 'white',
+          marginRight: '1rem'
+        }}
+      >
+        +
+      </button>
+      <span style={{ marginRight: '1rem' }}>0</span>
+      <button
+        style={{
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: 'white',
+          marginRight: '1rem'
+        }}
+      >
+        -
+      </button>
+      <button
+        style={{
+          padding: '0.375rem 0.75rem',
+          border: '1px solid black',
+          borderRadius: '4px',
+          backgroundColor: '#555555',
+          color: 'white',
+          marginRight: '1rem'
+        }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default App;
