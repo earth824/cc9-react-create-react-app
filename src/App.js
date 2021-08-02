56ca8e2300b01ca97ceff3933c1e1822d420b5f7
pisutthee <pisut.flyinggiraffe@gmail.com>
2021-08-02 11:10:32 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -2,44 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <button
-        style={{
-          padding: '0.375rem 0.75rem',
-          border: '1px solid black',
-          borderRadius: '4px',
-          backgroundColor: '#555555',
-          color: 'white',
-          marginRight: '1rem'
-        }}
-      >
-        +
-      </button>
-      <span style={{ marginRight: '1rem' }}>0</span>
-      <button
-        style={{
-          padding: '0.375rem 0.75rem',
-          border: '1px solid black',
-          borderRadius: '4px',
-          backgroundColor: '#555555',
-          color: 'white',
-          marginRight: '1rem'
-        }}
-      >
-        -
-      </button>
-      <button
-        style={{
-          padding: '0.375rem 0.75rem',
-          border: '1px solid black',
-          borderRadius: '4px',
-          backgroundColor: '#555555',
-          color: 'white',
-          marginRight: '1rem'
-        }}
-      >
-        Reset
-      </button>
+    <div className="counter">
+      <button className="btn">+</button>
+      <span className="current">0</span>
+      <button className="btn">-</button>
+      <button className="btn">Reset</button>
     </div>
   );
 }
